1f7565d8b28c4397ef0b0d4ae6f413dad3fc57f5
roisu <roisusi@gmail.com>
2021-03-08 19:32:50 +0200

    Fragment Desing first stage

--- RRsZoo/app/src/main/java/com/example/rrszoo/MainActivity.java
+++ RRsZoo/app/src/main/java/com/example/rrszoo/MainActivity.java
@@ -6,15 +6,24 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
     private FragmentManager fragmentManager;
+    private Button login;
+    private Button register;
+    private Button guest;
+    private ImageView title;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
 
@@ -23,7 +32,15 @@ public class MainActivity extends AppCompatActivity {
     public void loginFrag (View view){
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.LoginFrag,new FragmentLogin()).commit();
+        fragmentTransaction.add(R.id.fragment,new FragmentLogin()).commit();
+        login = (Button) findViewById(R.id.login);
+        login.setVisibility(View.INVISIBLE);
+        register = (Button) findViewById(R.id.register);
+        register.setVisibility(View.INVISIBLE);
+        guest = (Button) findViewById(R.id.guest);
+        guest.setVisibility(View.INVISIBLE);
+        title = (ImageView) findViewById(R.id.titleBar);
+        title.setVisibility(View.INVISIBLE);
 
     }
 
