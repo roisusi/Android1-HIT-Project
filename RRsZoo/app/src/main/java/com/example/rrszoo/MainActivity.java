8be7018357bd868c81b8a6a79164111836c03d3d
roisu <roisusi@gmail.com>
2021-03-08 21:53:13 +0200

    Fragment Done

--- RRsZoo/app/src/main/java/com/example/rrszoo/MainActivity.java
+++ RRsZoo/app/src/main/java/com/example/rrszoo/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 
+
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "MainActivity";
     private FragmentManager fragmentManager;
     private Button login;
     private Button register;
@@ -22,27 +25,43 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.login);
+        register = (Button) findViewById(R.id.register);
+        guest = (Button) findViewById(R.id.guest);
+        title = (ImageView) findViewById(R.id.titleBar);
 
 
     }
 
 
-
-
-    public void loginFrag (View view){
+    public void loginFrag(View view) {
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment,new FragmentLogin()).commit();
-        login = (Button) findViewById(R.id.login);
+        fragmentTransaction.add(R.id.fragment, new FragmentLogin()).addToBackStack(null).commit();
+
+
         login.setVisibility(View.INVISIBLE);
-        register = (Button) findViewById(R.id.register);
         register.setVisibility(View.INVISIBLE);
-        guest = (Button) findViewById(R.id.guest);
         guest.setVisibility(View.INVISIBLE);
-        title = (ImageView) findViewById(R.id.titleBar);
         title.setVisibility(View.INVISIBLE);
 
     }
 
+    ///backToMain
+    public void backToMain(View view) {
+        Log.d(TAG, "onBackPressed: ");
+        if (fragmentManager.getBackStackEntryCount() == 0) {
+            this.finish();
+        } else {
+            fragmentManager.popBackStack();
+            login.setVisibility(View.VISIBLE);
+            register.setVisibility(View.VISIBLE);
+            guest.setVisibility(View.VISIBLE);
+            title.setVisibility(View.VISIBLE);
+        }
+    }
+
 
 }
+
+
