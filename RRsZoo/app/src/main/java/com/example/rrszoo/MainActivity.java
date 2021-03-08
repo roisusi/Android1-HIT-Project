d64c6f572455526cbeff2773f223b4e8fc3c09e3
roisu <roisusi@gmail.com>
2021-03-08 19:05:27 +0200

    FirstFragment Tryout

--- RRsZoo/app/src/main/java/com/example/rrszoo/MainActivity.java
+++ RRsZoo/app/src/main/java/com/example/rrszoo/MainActivity.java
@@ -1,42 +1,31 @@
 package com.example.rrszoo;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.AsyncTask;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
-    String message = "";
-    EditText et;
-    myTask mt;
+    private FragmentManager fragmentManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        et = (EditText) findViewById(R.id.EditText01);
-
-    }
-
-    public void ConnectToDataBase(View view){
-        message = "connect";
-        mt = new myTask(message);
-        mt.execute();
-    }
-
-    public void onClick(View view) {
-        message = et.getText().toString();
-        mt = new myTask(message);
-        mt.execute();
-        Toast.makeText(getApplicationContext(),"Data sent",Toast.LENGTH_LONG);
     }
 
 
+
+
+    public void loginFrag (View view){
+        fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.LoginFrag,new FragmentLogin()).commit();
+
+    }
 
 
 }
